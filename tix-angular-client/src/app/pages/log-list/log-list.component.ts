5a294df5e09ea3305cf067e3bc758b81cd67c022
winqus <67459222+winqus@users.noreply.github.com>
2025-03-04 10:50:12 +0200

    Update Logs service, add Fake and Azure implementations, add scrollable loading in log-list component

--- tix-angular-client/src/app/pages/log-list/log-list.component.ts
+++ tix-angular-client/src/app/pages/log-list/log-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ErrorHandlerService } from '@ntx/app/shared/services/errorHandler.service';
 import { LogsService, TableData } from '@ntx/app/shared/services/logs.service';
+import { environment } from '@ntx/environments/environment';
 
 @Component({
   selector: 'app-log-list',
@@ -8,14 +10,62 @@ import { LogsService, TableData } from '@ntx/app/shared/services/logs.service';
   templateUrl: './log-list.component.html',
   styles: ``,
 })
-export class LogListComponent {
+export class LogListComponent implements OnInit {
   columns: string[] = [];
-  data: (TableData & Record<string, string>)[] = [];
+  logs: (TableData & Record<string, string>)[] = [];
+  currentPage = 1;
+  totalPages = 1;
+  isLoading = false;
 
-  constructor(private service: LogsService) {}
+  constructor(
+    private service: LogsService,
+    private readonly errorHandler: ErrorHandlerService
+  ) {}
 
   ngOnInit(): void {
     this.service.getColumns().subscribe((cols) => (this.columns = cols));
-    this.service.getData().subscribe((data) => (this.data = data));
+    this.loadLogs();
+  }
+
+  loadLogs() {
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+
+    this.service.getLogs(this.currentPage).subscribe({
+      next: (response) => {
+        if (environment.development) console.log('Get logs:', response);
+
+        if (this.currentPage === 1) {
+          this.logs = response.logs;
+        } else {
+          this.logs = [...this.logs, ...response.logs];
+        }
+        // const uniqueLogIds = new Set(this.logs.map((log) => log.LineId));
+        // this.logs = this.logs.filter((log) => uniqueLogIds.has(log.LineId));
+
+        this.totalPages = response.totalPages;
+        this.isLoading = false;
+      },
+      error: (errorResponse) => {
+        if (environment.development) console.error('Error getting data:', errorResponse);
+        this.errorHandler.showError('Please try again later.', 'Initial server error');
+        this.isLoading = false;
+      },
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const threshold = 2000;
+    const position = window.innerHeight + window.scrollY;
+    const height = document.body.scrollHeight;
+
+    if (position >= height - threshold) {
+      if (!this.isLoading && this.currentPage < this.totalPages) {
+        this.currentPage++;
+        this.loadLogs();
+      }
+    }
   }
 }
--- tix-angular-client/src/app/shared/services/logs.service.ts
+++ tix-angular-client/src/app/shared/services/logs.service.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { getLogColumns, getLogsOnPage } from '../config/api-endpoints';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@ntx/environments/environment';
 
 export interface TableData {
   LineId: number;
@@ -12,68 +16,143 @@ export interface TableData {
   Duration_ms: string;
 }
 
-@Injectable({
-  providedIn: 'root',
-})
-export class LogsService {
+class FakeLogsService implements LogsService {
   constructor() {}
 
-  getColumns(): Observable<string[]> {
+  private fakeLogsArray = [
+    {
+      LineId: 1,
+      Date: '2024-12-04',
+      Time: '11:00:53',
+      Level: 'LOG',
+      Node: 'NTX-Nest',
+      Component: 'NestFactory',
+      Content: 'Starting Nest application...',
+      Duration_ms: '0',
+    },
+    {
+      LineId: 2,
+      Date: '2024-12-04',
+      Time: '11:00:53',
+      Level: 'LOG',
+      Node: 'NTX-Nest',
+      Component: 'InstanceLoader',
+      Content: 'JobQueueModule dependencies initialized',
+      Duration_ms: '4',
+    },
+    {
+      LineId: 3,
+      Date: '2024-12-04',
+      Time: '11:00:53',
+      Level: 'LOG',
+      Node: 'NTX-Nest',
+      Component: 'InstanceLoader',
+      Content: 'BullBoardModule dependencies initialized',
+      Duration_ms: '1',
+    },
+    {
+      LineId: 4,
+      Date: '2024-12-04',
+      Time: '11:00:53',
+      Level: 'LOG',
+      Node: 'NTX-Nest',
+      Component: 'InstanceLoader',
+      Content: 'FileStorageModule dependencies initialized',
+      Duration_ms: '0',
+    },
+    {
+      LineId: 5,
+      Date: '2024-12-04',
+      Time: '11:00:53',
+      Level: 'LOG',
+      Node: 'NTX-Nest',
+      Component: 'InstanceLoader',
+      Content: 'JobQueueModule dependencies initialized',
+      Duration_ms: '1',
+    },
+  ];
+
+  public getColumns(): Observable<string[]> {
     return of(['LineId', 'Date', 'Time', 'Level', 'Node', 'Component', 'Content', 'Duration_ms']);
   }
 
-  getData(): Observable<any> {
-    return of([
-      {
-        LineId: 1,
-        Date: '2024-12-04',
-        Time: '11:00:53',
-        Level: 'LOG',
-        Node: 'NTX-Nest',
-        Component: 'NestFactory',
-        Content: 'Starting Nest application...',
-        Duration_ms: '0',
-      },
-      {
-        LineId: 2,
-        Date: '2024-12-04',
-        Time: '11:00:53',
-        Level: 'LOG',
-        Node: 'NTX-Nest',
-        Component: 'InstanceLoader',
-        Content: 'JobQueueModule dependencies initialized',
-        Duration_ms: '4',
-      },
-      {
-        LineId: 3,
-        Date: '2024-12-04',
-        Time: '11:00:53',
-        Level: 'LOG',
-        Node: 'NTX-Nest',
-        Component: 'InstanceLoader',
-        Content: 'BullBoardModule dependencies initialized',
-        Duration_ms: '1',
-      },
-      {
-        LineId: 4,
-        Date: '2024-12-04',
-        Time: '11:00:53',
-        Level: 'LOG',
-        Node: 'NTX-Nest',
-        Component: 'InstanceLoader',
-        Content: 'FileStorageModule dependencies initialized',
-        Duration_ms: '0',
-      },
-      {
-        LineId: 5,
-        Date: '2024-12-04',
-        Time: '11:00:53',
-        Level: 'LOG',
-        Node: 'NTX-Nest',
-        Component: 'InstanceLoader',
-        Content: 'JobQueueModule dependencies initialized',
-        Duration_ms: '1',
-      },
-    ]);
+  public getLogs(page?: number): Observable<{
+    logs: any[];
+    page: number;
+    totalPages: number;
+  }> {
+    page = page ?? 1;
+
+    return of({
+      logs: [this.fakeLogsArray, this.fakeLogsArray, this.fakeLogsArray, this.fakeLogsArray, this.fakeLogsArray].flat(),
+      page,
+      totalPages: 20,
+    });
   }
+}
+
+class AzureFunctionsLogsService implements LogsService {
+  constructor(private readonly http: HttpClient) {}
+
+  public getColumns(): Observable<string[]> {
+    const url = getLogColumns();
+    const httpOptions = {};
+
+    return this.http.get(url, httpOptions).pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error) => {
+        if (environment.development) console.error('Error fetching log columns:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  public getLogs(page?: number): Observable<{
+    logs: any[];
+    page: number;
+    totalPages: number;
+  }> {
+    page = page ?? 1;
+    const url = getLogsOnPage(page);
+    const httpOptions = {};
+
+    return this.http.get(url, httpOptions).pipe(
+      map((response: any) => {
+        const { page, limit, totallogs, totalPages, logs } = response;
+
+        return {
+          logs,
+          page,
+          totalPages,
+        };
+      }),
+      catchError((error) => {
+        if (environment.development) console.error('Error fetching logs:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+  deps: [HttpClient],
+  useFactory: (http: HttpClient) => {
+    if (environment.development) {
+      return new FakeLogsService();
+    } else {
+      return new AzureFunctionsLogsService(http);
+    }
+  },
+})
+export abstract class LogsService {
+  abstract getColumns(): Observable<string[]>;
+
+  abstract getLogs(page?: number): Observable<{
+    logs: any[];
+    page: number;
+    totalPages: number;
+  }>;
 }
